7c5d412a17c48ab1601a1b26110eb57142dfa3b4
YoelMedrano <39170047+YoelMedrano@users.noreply.github.com>
2018-06-14 12:09:41 -0400

    Update services.js

--- modules/authentication/services.js
+++ modules/authentication/services.js
@@ -22,7 +22,7 @@ angular.module('Authentication')
 
             // Use this for real authentication
              //----------------------------------------------
-            $http.post( 'https://proyectois.herokuapp.com/account/login', { email: email, password: password })
+            $http.post( "https://proyectois.herokuapp.com/account/login", { email: email, password: password })
                 .success(function (response) {
                     callback(response);
               });
